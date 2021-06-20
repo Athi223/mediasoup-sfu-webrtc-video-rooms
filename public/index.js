ffca32db06fa2ddf88420454a1d1b0b6cf8954a1
Atharv Phadnis <athiphadnis@gmail.com>
2021-06-20 20:27:27 +0530

    Improved Interface

--- public/index.js
+++ public/index.js
@@ -2,110 +2,70 @@ if (location.href.substr(0, 5) !== 'https')
   location.href = 'https' + location.href.substr(4, location.href.length - 4)
 
 const socket = io()
-
-
 let producer = null;
+let rc = null
 
 nameInput.value = 'bob' + Math.round(Math.random() * 1000)
 
 socket.request = function request(type, data = {}) {
-  return new Promise((resolve, reject) => {
-    socket.emit(type, data, (data) => {
-      if (data.error) {
-        reject(data.error)
-      } else {
-        resolve(data)
-      }
-    })
-  })
+	return new Promise((resolve, reject) => {
+		socket.emit(type, data, (data) => {
+			if (data.error) {
+			reject(data.error)
+			} else {
+			resolve(data)
+			}
+		})
+	})
 }
 
-let rc = null
 
 function joinRoom(name, room_id) {
   if (rc && rc.isOpen()) {
-    console.log('already connected to a room')
+	console.log('already connected to a room')
   } else {
-    rc = new RoomClient(localMedia, remoteVideos, remoteAudios, window.mediasoupClient, socket, room_id, name, roomOpen)
+	rc = new RoomClient(localMedia, remoteVideos, remoteAudios, window.mediasoupClient, socket, room_id, name, roomOpen)
 
-    addListeners()
+	addListeners()
   }
 
 }
 
 function roomOpen() {
-  login.className = 'hidden'
-  reveal(startAudioButton)
-  hide(stopAudioButton)
-  reveal(startVideoButton)
-  hide(stopVideoButton)
-  reveal(startScreenButton)
-  hide(stopScreenButton)
-  reveal(exitButton)
-  control.className = ''
-  reveal(videoMedia)
+	login.classList.add('d-none')
+	call.classList.remove('d-none')
 }
-
-function hide(elem) {
-  elem.className = 'hidden'
-}
-
-function reveal(elem) {
-  elem.className = ''
-}
-
 
 function addListeners() {
-  rc.on(RoomClient.EVENTS.startScreen, () => {
-    hide(startScreenButton)
-    reveal(stopScreenButton)
-  })
+	rc.on(RoomClient.EVENTS.startScreen, () => {
+		startScreenButton.classList.add('d-none')
+		stopScreenButton.classList.remove('d-none')
+	})
 
-  rc.on(RoomClient.EVENTS.stopScreen, () => {
-    hide(stopScreenButton)
-    reveal(startScreenButton)
+	rc.on(RoomClient.EVENTS.stopScreen, () => {
+		stopScreenButton.classList.add('d-none')
+		startScreenButton.classList.remove('d-none')
+	})
 
-  })
+	rc.on(RoomClient.EVENTS.stopAudio, () => {
+		stopAudioButton.classList.add('d-none')
+		startAudioButton.classList.remove('d-none')
+	})
+		rc.on(RoomClient.EVENTS.startAudio, () => {
+		startAudioButton.classList.add('d-none')
+		stopAudioButton.classList.remove('d-none')
+	})
 
-  rc.on(RoomClient.EVENTS.stopAudio, () => {
-    hide(stopAudioButton)
-    reveal(startAudioButton)
-
-  })
-  rc.on(RoomClient.EVENTS.startAudio, () => {
-    hide(startAudioButton)
-    reveal(stopAudioButton)
-  })
-
-  rc.on(RoomClient.EVENTS.startVideo, () => {
-    hide(startVideoButton)
-    reveal(stopVideoButton)
-  })
-  rc.on(RoomClient.EVENTS.stopVideo, () => {
-    hide(stopVideoButton)
-    reveal(startVideoButton)
-  })
-  rc.on(RoomClient.EVENTS.exitRoom, () => {
-    hide(control)
-    reveal(login)
-    hide(videoMedia)
-  })
+	rc.on(RoomClient.EVENTS.startVideo, () => {
+		startVideoButton.classList.add('d-none')
+		stopVideoButton.classList.remove('d-none')
+	})
+		rc.on(RoomClient.EVENTS.stopVideo, () => {
+		stopVideoButton.classList.add('d-none')
+		startVideoButton.classList.remove('d-none')
+	})
+	rc.on(RoomClient.EVENTS.exitRoom, () => {
+		call.classList.add('d-none')
+		login.classList.remove('d-none')
+	})
 }
-
-// Load mediaDevice options
-navigator.mediaDevices.enumerateDevices().then(devices =>
-  devices.forEach(device => {
-    let el = null
-    if ('audioinput' === device.kind) {
-      el = audioSelect
-    } else if ('videoinput' === device.kind) {
-      el = videoSelect
-    }
-    if(!el) return
-
-    let option = document.createElement('option')
-    option.value = device.deviceId
-    option.innerText = device.label
-    el.appendChild(option)
-  })
-)
